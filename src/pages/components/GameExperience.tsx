13ffd84fc58b054d17ce482076d5ff4cfa47999c
Dennis <dennis@momentum.local>
2023-07-28 10:33:06 -0700

    green cube

---
+++ src/pages/components/GameExperience.tsx
@@ -0,0 +1,12 @@
+import { OrbitControls } from "@react-three/drei";
+import Level from "./Level";
+
+const GameExperience = () => {
+  return (
+    <>
+      <Level />
+    </>
+  );
+};
+
+export default GameExperience;
